3f5d31d36c570b771ccccefc94704fbf6817b224
Dzmitry Kulik <dzmitser.kulik@gmail.com>
2021-09-11 16:07:54 +0300

    constructors for AuthRequest model added

--- src/main/java/com/kulik/airbnb/model/AuthRequest.java
+++ src/main/java/com/kulik/airbnb/model/AuthRequest.java
@@ -4,9 +4,16 @@ public class AuthRequest {
     private String email;
     private String password;
 
+    public AuthRequest() {}
+
     public AuthRequest(String email, String password) {
         this.email = email;
         this.password = password;
+    }
+
+    public AuthRequest(AuthRequest auth) {
+        this.email = auth.email;
+        this.password = auth.password;
     }
 
     public String getPassword() {
